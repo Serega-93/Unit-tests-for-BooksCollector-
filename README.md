b41b3cd1353ec08150c982d0880bb8e13506bb7a
Sergei Filipchenko <s.e.r.e.g.a.-93@mail.ru>
2025-04-07 19:41:09 +0500

    Created unit-tests

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# qa_python
+# Покрытие юнит-тестами класса "BooksCollector"
+# Удалось реализовать такие автотесты:
+# test_add_new_book_add_two_books - добавление двух книг
+# test_add_new_book_check_positive_boundary_values - добавление книг с количеством содержания символов на границах допустимого диапазона от 1 до 40
+# test_set_book_genre_set_new_book_genre - добавляем книги жанр
+# test_get_book_genre_new_book_not_genre - у добаленной книги нет жанра
+# test_get_books_with_specific_genre_get_book_by_genre - получение книги по жанру
+# test_get_books_for_children_get_books - возвращение книг подходящих детям
+# test_add_book_in_favorites_add_book - добавление книги в избранное
+# test_delete_book_from_favorites_delete_book - удаление книги из избранного
+# test_add_new_book_add_book_again - проверяем что одна и та же книга не добавляется ещё раз
--- tests.py
+++ tests.py
@@ -1,24 +1,73 @@
+import pytest
+
+from data import BOOK_NAME, GENRE
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
     def test_add_new_book_add_two_books(self):
-        # создаем экземпляр (объект) класса BooksCollector
         collector = BooksCollector()
-
-        # добавляем две книги
         collector.add_new_book('Гордость и предубеждение и зомби')
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        assert len(collector.get_books_genre()) == 2
 
-        # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+    # проверяем добавление книг с количеством содержания символов на границах
+    # допустимого диапазона от 1 до 40
+    @pytest.mark.parametrize('values', ['а',# 1 символ
+                                      'ааааааааааааааааааааааааааааааааааааааа',# 39 символов
+                                      'аааааааааааааааааааааааааааааааааааааааа'])# 40 символов
+    def test_add_new_book_check_positive_boundary_values(self, collector, values):
+        collector.add_new_book(values)
+        book_name = collector.get_books_genre()
+        assert  values in book_name
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+    # проверяем что новой книге устанавливается жанр
+    def test_set_book_genre_set_new_book_genre(self, collector):
+        collector.add_new_book(BOOK_NAME[0])
+        collector.set_book_genre(BOOK_NAME[0], GENRE[0])
+        assert collector.get_book_genre(BOOK_NAME[0]) == GENRE[0]
+
+    # проверяем что у добавленной книге нет жанра
+    def test_get_book_genre_new_book_not_genre(self, collector):
+        collector.add_new_book(BOOK_NAME[0])
+        assert collector.get_book_genre(BOOK_NAME[0]) == ''
+
+    # проверяем получение книги по жанру
+    def test_get_books_with_specific_genre_get_book_by_genre(self, collector):
+        collector.add_new_book(BOOK_NAME[1])
+        collector.set_book_genre(BOOK_NAME[1], GENRE[4])
+        assert  collector.get_books_with_specific_genre(GENRE[4]) == [BOOK_NAME[1]]
+
+    # проверяем возвращение книг подходящих детям
+    def test_get_books_for_children_get_books(self, collector):
+        collector.add_new_book(BOOK_NAME[3])
+        collector.add_new_book(BOOK_NAME[2])
+        collector.set_book_genre(BOOK_NAME[3], GENRE[3])
+        collector.set_book_genre(BOOK_NAME[2], GENRE[1])
+        assert  collector.get_books_for_children() == [BOOK_NAME[3]]
+
+    # проверяем добавление книги в избранное
+    def test_add_book_in_favorites_add_book(self, collector):
+        collector.add_new_book(BOOK_NAME[0])
+        collector.add_book_in_favorites(BOOK_NAME[0])
+        assert collector.get_list_of_favorites_books() == [BOOK_NAME[0]]
+
+    # проверяем удаление книги из избранного
+    def test_delete_book_from_favorites_delete_book(self, collector):
+        collector.add_new_book(BOOK_NAME[0])
+        collector.add_book_in_favorites(BOOK_NAME[0])
+        collector.delete_book_from_favorites(BOOK_NAME[0])
+        assert collector.get_list_of_favorites_books() == []
+
+    # проверяем что одна и та же книга не добавляется ещё раз
+    def test_add_new_book_add_book_again(self, collector):
+        collector.add_new_book(BOOK_NAME[0])
+        collector.add_new_book(BOOK_NAME[0])
+        book_list = collector.get_books_genre()
+        assert  len(book_list) == 1
+
+
+
+
+
